d0f681c116f4744b2b6b0c7fb8ad42c56fd89d64
MasterUNG <phrombutr@gmail.com>
2018-03-03 15:29:17 +0700

    Register Success

--- app/src/main/java/masterung/androidthai/in/th/qrfirebase/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/qrfirebase/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import masterung.androidthai.in.th.qrfirebase.R;
 
@@ -14,6 +15,31 @@ import masterung.androidthai.in.th.qrfirebase.R;
  */
 
 public class MainFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        registerController();
+
+
+    }   // Main Method
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtNewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
 
     @Nullable
     @Override
