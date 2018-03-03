a02d3dd482a5a370734f89577b90c8f36c9e6ca4
MasterUNG <phrombutr@gmail.com>
2018-03-03 15:48:23 +0700

    Check Space Main Success

--- app/src/main/java/masterung/androidthai/in/th/qrfirebase/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/qrfirebase/fragment/MainFragment.java
@@ -6,9 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import masterung.androidthai.in.th.qrfirebase.R;
+import masterung.androidthai.in.th.qrfirebase.utility.MainAlert;
 
 /**
  * Created by masterung on 3/3/2018 AD.
@@ -23,8 +26,35 @@ public class MainFragment extends Fragment{
 //        Register Controller
         registerController();
 
+//        Login Controller
+        loginController();
 
     }   // Main Method
+
+    private void loginController() {
+        Button button = getView().findViewById(R.id.btnLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText emailEditText = getView().findViewById(R.id.edtEmail);
+                EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+                String emailString = emailEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if (emailString.isEmpty() || passwordString.isEmpty()) {
+                    MainAlert mainAlert = new MainAlert(getActivity());
+                    mainAlert.normalDialog("Have Space",
+                            "Please Fill All Blank");
+                } else {
+
+                }
+
+
+            }
+        });
+    }
 
     private void registerController() {
         TextView textView = getView().findViewById(R.id.txtNewRegister);
